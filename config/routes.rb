9334aa988a2a6e7a271685cdbc2cd04703329b9a
Lugard Agu <agulugard@gmail.com>
2023-08-08 14:47:06 +0100

    Update devise and home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Defines the root path route ("/")
-  root to:"users#index"
-  # root to: "home#index"
 
-  resources :users
-  resources :foods, only: %i[index new create destroy]
-  resources :recipes, only: %i[index show new create destroy] do
-    resources :recipe_foods, only: %i[new create destroy] 
-  end
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  resources :home, only: [:index] 
 end
