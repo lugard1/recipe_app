47400dc5e3fb9677f51d256dfdc089e0a9b16536
Lugard Agu <agulugard@gmail.com>
2023-08-08 12:45:12 +0100

    Update user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, :surname, presence: true
+  validates :name, presence: true
 
   has_many :foods, foreign_key: 'user_id'
   has_many :recipes, foreign_key: 'user_id'
