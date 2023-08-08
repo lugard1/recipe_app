024ac80b9406ccbf901e7a63e1d637fc40f2aadf
Lugard Agu <agulugard@gmail.com>
2023-08-08 09:51:42 +0100

    Create database models and migrations for user food recipe and recipe_food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to:"users#index"
+
+  resources :users
+  resources :foods, only: %i[index new create destroy]
+  resources :recipes, only: %i[index show new create destroy] do
+    resources :recipe_foods, only: %i[new create destroy] 
+  end
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
