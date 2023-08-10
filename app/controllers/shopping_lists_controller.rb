d6c4483b02da9164c1e6927054ad89184c7e7b19
Lugard Agu <agulugard@gmail.com>
2023-08-10 19:03:34 +0100

    Update shopping_lists controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,11 +1,11 @@
 class ShoppingListsController < ApplicationController
   def index
-    @foods = Food.where(user_id_id: current_user.id).order(created_at: :desc)
-    @recipes = Recipe.where(user_id_id: current_user.id)
+    @foods = Food.where(user_id: current_user.id).order(created_at: :desc)
+    @recipes = Recipe.where(user_id: current_user.id)
 
     array = @recipes.map do |recipe|
       recipe.recipe_foods.map do |food|
-        { id: food.food_id_id, quantity: food.quantity }
+        { id: food.food_id, quantity: food.quantity }
       end
     end
     @foody = array.flatten.map { |hash| { id: hash[:id], quantity: hash[:quantity] } }
@@ -24,7 +24,7 @@ class ShoppingListsController < ApplicationController
         measurement_unit: row.measurement_unit,
         price: row.price,
         quantity: row.quantity,
-        user_id_id: row.user_id_id,
+        user_id: row.user_id,
         created_at: row.created_at,
         updated_at: row.updated_at
       }
